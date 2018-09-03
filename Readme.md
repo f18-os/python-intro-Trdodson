39bc4e820483d2020a2fc68e8c906e2d361495ab
Taylor Dodson <t.raynedod@gmail.com>
2018-09-03 10:56:08 -0600

    Word count fully implemented. Readme updated with description and refrences.

--- Readme.md
+++ Readme.md
@@ -1,3 +1,60 @@
+# Lab 1: Word Counter
+
+## Introduction
+
+This is a simple word counting program. The program takes two text file names
+- one being the file to be counted, the other being the output of the count -
+counts the words, and then outputs a text file containing each word used in the file and the number of times they are used.
+
+This directory contains:
+ * The counting program, wordCount.py.
+ * A program to test the accuracy of wordCount.py.
+ * Text files, including "keys" for the program to read.
+
+## How To Use
+
+The program is executed from the command line. The user specifies the file
+they want to count for and the desired name of the consequent output
+file. Start the program using the following input:
+
+~~~
+$ python wordCount.py <input file> <output file>
+
+~~~
+Test the accuracy of the program using wordCountTest.py. This program takes
+the file to be counted, the already generated output file, and one of the
+"key" files, all of which already contain the correct output for the provided
+text files.
+
+~~~
+$ python wordCountTest.py <input file><output file><solution key file>
+
+~~~
+
+## Refrences
+
+This assignment was prepared in a manner consistent with the instructor's
+requirements. All significant collaboration or guidance from external sources is clearly documented. These include:
+
+https://stackoverflow.com/questions/16922214/reading-a-text-file-and-splitting-it-into-single-words-in-python
+- For understanding of syntax for splitting a line into single words in Python.
+
+ https://stackoverflow.com/questions/1602934/check-if-a-given-key-already-exists-in-a-dictionary
+ - For understanding syntax for locating items in dictionaries in Python.
+
+https://www.guru99.com/reading-and-writing-files-in-python.html for
+understanding write() - For understanding implementation of read() and write().
+
+https://www.geeksforgeeks.org/sorted-function-python/ - For understanding the
+sorted() function.
+
+https://docs.python.org/3/tutorial/datastructures.html#looping-techniques -
+For iterating through a dictionary.
+
+File checks, imports, "with open(...)" syntax, and implemtation of function arguments refrenced from wordCountTest.py, as provided by the instructor.
+
+## Original Lab Instructions
+
 This repository contains the code for the python introduction lab. The
 purpose is to have a fairly simple python assignment that introduces
 the basic features and tools of python
--- wordCount.py
+++ wordCount.py
@@ -1,29 +1,48 @@
-#! /usr/bin/env python3
+#!y /usr/bin/env python3
+
+#A simple word counter. Takes two file names as input, reads one, and then writes all words with their counts to second file.
+#This assignment was prepared in a manner consistent with the instructor's requirements. All significant collaboration or guidance from external sources is clearly documented; see Readme for details.
 
 import sys        # command line arguments
 import re         # regular expression tools
+import os         # checking if file exists
 
-# set input and output files and check if input is valid km- code modified from wordCountTest.py.
+# set input and output files and check if input is valid. Code modified from wordCountTest.py.
 if len(sys.argv) is not 3:
-    print("Correct usage: wordCount.py <input text file> <output file> ")
+    print("Erroneous input. Correct usage: wordCount.py <input file> <output file> ")
     exit()
 
-inputName = sys.argv[1]
-outputName = sys.argv[2]
-words = {}
+inputName = sys.argv[1]  # Save name of input file.
+outputName = sys.argv[2] # Save name of output file
+words = {}               # Words and counts will be stored here.
 
-#Opens file and counts the words! Modified from wordCountTest.py and stack overflow.
-#(See https://stackoverflow.com/questions/16922214/reading-a-text-file-and-splitting-it-into-single-words-in-python)
+
+# Make sure the input file exists! Modified from wordCountTest.py.
+if not os.path.exists(inputName):
+    print ("Text file %s doesn't exist! Exiting..." % inputName)
+    exit()
+
+
+# Opens file and counts the words! Modified from wordCountTest.py and stack overflow.
+# Refrenced https://stackoverflow.com/questions/16922214/reading-a-text-file-and-splitting-it-into-single-words-in-python
+# Refrenced https://stackoverflow.com/questions/1602934/check-if-a-given-key-already-exists-in-a-dictionary
+
 with open(inputName, 'r') as myFile:
-    for line in myFile:
-        line = line.strip()
-        for w in re.split('[ \t.,-:;]', line):
+    for line in myFile:                          # For every line in the file:
+        line = line.strip()                      # Delete newline chars.
+        for w in re.split("['\s\t.,-;:]", line): # For every word in the line (split at spaces & punctuation):
+            if w.lower() == "":                  #Don't count whitespace. lower() for case insenstivity.
+                continue
             if w.lower() in words:
-                words[w] += 1
+                words[w.lower()] += 1
             else:
-                words[w] = 1
+                words[w.lower()] = 1
                 
-#Write to output file. Refrenced https://www.guru99.com/reading-and-writing-files-in-python.html
+# Write to the output file.
+# Refrenced https://www.guru99.com/reading-and-writing-files-in-python.html for understanding write()
+# Refrenced https://www.geeksforgeeks.org/sorted-function-python/
+# Refrenced https://docs.python.org/3/tutorial/datastructures.html#looping-techniques
+
 f = open(outputName,"w+")
 for w, n in sorted(words.items()):
     f.write(w + " " + str(n) + "\n")
