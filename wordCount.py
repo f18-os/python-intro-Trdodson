2225de3c53396dd4881600ce34edfc5e9ec6fdc5
Taylor Dodson <t.raynedod@gmail.com>
2018-09-02 23:27:43 -0600

    Word count partially implemented. Sorting and case insensitivity still bugged.

--- wordCount.py
+++ wordCount.py
@@ -3,24 +3,29 @@
 import sys        # command line arguments
 import re         # regular expression tools
 
-# set input and output files - code modified from wordCountTest.py.
+# set input and output files and check if input is valid km- code modified from wordCountTest.py.
 if len(sys.argv) is not 3:
     print("Correct usage: wordCount.py <input text file> <output file> ")
     exit()
 
 inputName = sys.argv[1]
 outputName = sys.argv[2]
-count = 0
+words = {}
 
-#Opens file and counts the words! Modified from wordCountTest.py.
+#Opens file and counts the words! Modified from wordCountTest.py and stack overflow.
+#(See https://stackoverflow.com/questions/16922214/reading-a-text-file-and-splitting-it-into-single-words-in-python)
 with open(inputName, 'r') as myFile:
     for line in myFile:
-        for w in line.split():
-            count += 1
-        
-
-#Print number of lines and create outputfile.
-print(count)
-f= open(outputName,"w+")
+        line = line.strip()
+        for w in re.split('[ \t.,-:;]', line):
+            if w.lower() in words:
+                words[w] += 1
+            else:
+                words[w] = 1
+                
+#Write to output file. Refrenced https://www.guru99.com/reading-and-writing-files-in-python.html
+f = open(outputName,"w+")
+for w, n in sorted(words.items()):
+    f.write(w + " " + str(n) + "\n")
 
 f.close()
