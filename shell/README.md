6de25f62597cf60c2d85bfae69ce06bc965d4d68
Taylor Dodson <t.raynedod@gmail.com>
2018-09-10 08:24:48 -0600

    User can run commands on shell.py (like cat and wc). README updated.

--- shell/README.md
+++ shell/README.md
@@ -1,7 +1,8 @@
 # Basic Shell
 
-This directory contains a basic shell program. Currently, it is able to fork a
-clone child process and search directories for a specific program (wc).
+This directory contains a basic shell program. Currently, it is able to take
+input from a user, fork a child process and run the program specified by the
+user on the file shell.py.
 
 Code based on p3-exec program provided by the instructor.
 
--- shell/shell.py
+++ shell/shell.py
@@ -6,6 +6,9 @@ import os, sys, time, re
 
 pid = os.getpid()
 
+userCmd = input("Insert command to run on shell.py:")
+type(userCmd)
+
 os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
 
 rc = os.fork()
@@ -16,7 +19,7 @@ if rc < 0:
 
 elif rc == 0:
     os.write(1, ("I am child.  My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
-    args = ["wc", "shell.py"]
+    args = [userCmd, "shell.py"]
     for dir in re.split(":", os.environ['PATH']): # try each directory.
         program = "%s/%s" % (dir, args[0])        # path to program is here
         os.write(1, ("Child: ...trying to exec %s\n" % program).encode())
