3dccbad66f6c7f9719892086fd765105d9689ef8
Taylor Dodson <t.raynedod@gmail.com>
2018-09-10 07:49:16 -0600

    Implemented p3-exec. Updated README.

--- shell/README.md
+++ shell/README.md
@@ -1,5 +1,7 @@
 ##Basic Shell
-A basic shell program. Code heavily based on forking and exec programs
-provided by the instructor.
+This directory contains a basic shell program. Currently, it is able to fork a
+clone child process and search directories for a specific program (wc).
 
-Further edits as warranted - more details soon.
+Code based on p3-exec program provided by the instructor.
+
+Further edits as warranted - more details soon.y
--- shell/shell.py
+++ shell/shell.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python3
 
-#Basic shell. Code heavily based on code provided by instructor. See README for details.
+#Implementation for a basic shell. Code provided by instructor. See README for details.
 
-import os, sys
+import os, sys, time, re
 
 pid = os.getpid()
 
@@ -16,6 +16,17 @@ if rc < 0:
 
 elif rc == 0:
     os.write(1, ("I am child.  My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
-
+    args = ["wc", "shell.py"]
+    for dir in re.split(":", os.environ['PATH']): # try each directory.
+        program = "%s/%s" % (dir, args[0])        # path to program is here
+        os.write(1, ("Child: ...trying to exec %s\n" % program).encode())
+        try:
+            os.execve(program, args, os.environ) #Execute the program
+        except FileNotFoundError:
+            pass
+    os.write(2, ("Child: Could not exec %s \n" % program).encode())
+    sys.exit(1)
 else:
     os.write(1, ("I am parent.  My pid=%d.  Child's pid=%d\n" % (pid, rc)).encode())
+    childPidCode = os.wait() #Wait for child to die
+    os.write(1, ("Parent: Child %d terminated with exit code %d\n" % childPidCode).encode())
