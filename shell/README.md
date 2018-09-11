fa7d994bf2b88ef733c8b221baffe8bcd8725116
Taylor Dodson <t.raynedod@gmail.com>
2018-09-11 15:39:38 -0600

    User can now exit the shell on command.

--- shell/README.md
+++ shell/README.md
@@ -3,7 +3,7 @@
 This directory contains a basic shell program. Currently, it is able to take
 one command with one argument from a user, fork a child process and run the
 program specified. It also redirects the output of the program to a text file,
-shell-output.txt.
+shell-output.txt. The shell terminates if the user enters the string "exit".
 
 Code based on p3-exec and p4-redirect programs provided by the instructor.
 
--- shell/shell.py
+++ shell/shell.py
@@ -4,41 +4,46 @@
 
 import os, sys, time, re
 
-pid = os.getpid()
+while(True): # Keep asking for prompts 'till use types "exit"
 
-userCmd = input("Insert command>") # Prompt user for a command.
-type(userCmd)
+    pid = os.getpid()
+    userCmd = input("Insert command>") # Prompt user for a command.
+    type(userCmd)
 
-userArg = input("Insert argument>") # Prompt user for arguments.
-type(userArg)
+    if(userCmd == "exit"):
+        break
+
+    userArg = input("Insert argument>") # Prompt user for arguments
+    type(userArg)
     
-os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
+    os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
 
-rc = os.fork()
+    rc = os.fork()
 
-if rc < 0:
-    os.write(2, ("fork failed, returning %d\n" % rc).encode())
-    sys.exit(1)
+    if rc < 0:
+        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        sys.exit(1)
+    elif rc == 0:
+        
+        os.write(1, ("I am child.  My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
+        args = [userCmd, userArg]                    # Arguments for program
+        os.close(1)
+        sys.stdout = open("shell-output.txt", "w")   # Redirect output of program to text file.
+        fd = sys.stdout.fileno()                     # define file descriptor
+        os.set_inheritable(fd,True)
+        os.write(2,("Child: opened fd=%fd for writing\n" % fd).encode())
 
-elif rc == 0:
-    os.write(1, ("I am child.  My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
-    args = [userCmd, userArg]                    # Arguments for program
-
-    os.close(1)
-    sys.stdout = open("shell-output.txt", "w")   # Redirect output of program to text file.
-    fd = sys.stdout.fileno()                     # define file descriptor
-    os.set_inheritable(fd,True)
-    os.write(2,("Child: opened fd=%fd for writing\n" % fd).encode())
-    
-    for dir in re.split(":", os.environ['PATH']): # try each directory.
-        program = "%s/%s" % (dir, args[0])        # path to program is here
-        try:
-            os.execve(program, args, os.environ) #Execute the program
-        except FileNotFoundError:
-            pass
-    os.write(2, ("Child: Could not exec %s \n" % program).encode())
-    sys.exit(1)
-else:
-    os.write(1, ("I am parent.  My pid=%d.  Child's pid=%d\n" % (pid, rc)).encode())
-    childPidCode = os.wait() #Wait for child to die
-    os.write(1, ("Parent: Child %d terminated with exit code %d\n" % childPidCode).encode())
+        for dir in re.split(":", os.environ['PATH']): # try each directory.
+            program = "%s/%s" % (dir, args[0])        # path to program is here
+            try:
+                os.execve(program, args, os.environ) #Execute the program
+            except FileNotFoundError:
+                pass
+            
+        os.write(2, ("Child: Could not exec %s \n" % program).encode())
+        sys.exit(1)
+        
+    else:
+        os.write(1, ("I am parent.  My pid=%d.  Child's pid=%d\n" % (pid, rc)).encode())
+        childPidCode = os.wait() #Wait for child to die
+        os.write(1, ("Parent: Child %d terminated with exit code %d\n" % childPidCode).encode())
